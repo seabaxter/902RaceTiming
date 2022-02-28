0fcbb447d65a907405bee858481b63b68ba90cb9
Cody Baxter <codybaxter4@icloud.com>
2022-02-28 10:22:06 -0400

    first commit

---
+++ js/social-icons-widget-by-wpzoom-assets-js-social-icons-widget-frontend.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function ($) {
+    $('.zoom-social_icons-list__link').on({
+        'mouseenter': function (e) {
+            e.preventDefault();
+
+            var $this = $(this).find('.zoom-social_icons-list-span');
+            var $rule = $this.data('hover-rule');
+            var $color = $this.data('hover-color');
+            if ($color !== undefined) {
+                $this.attr('data-old-color', $this.css($rule));
+                $this.css($rule, $color);
+            }
+        },
+        'mouseleave': function (e) {
+            e.preventDefault();
+            var $this = $(this).find('.zoom-social_icons-list-span');
+            var $rule = $this.data('hover-rule');
+            var $oldColor = $this.data('old-color');
+            if ($oldColor !== undefined) {
+                $this.css($rule, $oldColor);
+            }
+        }
+    });
+});
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
